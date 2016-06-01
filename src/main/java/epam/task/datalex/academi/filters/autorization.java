c51c13843a00e73f773898d7ae60d9671cf0e5d3
mirror <mironosk@gmail.com>
2016-06-01 11:33:05 +0300

    Full

--- src/main/java/epam/task/datalex/academi/filters/autorization.java
+++ src/main/java/epam/task/datalex/academi/filters/autorization.java
@@ -14,7 +14,7 @@ public class autorization implements Filter {
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
         HttpSession session = ((HttpServletRequest) req).getSession(false);
         if (session == null || session.getAttribute("user") == null){
-            ((HttpServletResponse)resp).sendRedirect("/datalex.jsp");
+            ((HttpServletResponse)resp).sendRedirect("/startPage.jsp");
         }else {
             HttpServletResponse response = (HttpServletResponse)resp;
             chain.doFilter(req, resp);
